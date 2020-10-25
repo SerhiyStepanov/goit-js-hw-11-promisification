2cfc8c11a527b596bbc5251dc6bedd7cc411c4e6
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-10-25 19:26:16 +0200

    task-1 end

--- task-1.js
+++ task-1.js
@@ -1,10 +1,19 @@
 const delay = ms => {
-  // Твой код
+  const TIME = 1000;
+  return new Promise((resolved) => {
+    setTimeout(() => {
+    resolved(ms)
+  },TIME)
+  })
 };
 
 const logger = time => console.log(`Resolved after ${time}ms`);
 
 // Вызовы функции для проверки
-delay(2000).then(logger); // Resolved after 2000ms
+
 delay(1000).then(logger); // Resolved after 1000ms
+
 delay(1500).then(logger); // Resolved after 1500ms
+
+delay(2000).then(logger); // Resolved after 2000ms
+
